99bdf516fba859176fc4323344bb1472d414f711
ethan jiang <ethan1.jiang@gmail.com>
2020-06-17 14:04:56 +0800

    update the project: google-services, and new flutter/dart

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/annshsingh/Desktop/flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/annshsingh/Desktop/Widget Guide/FlutterWidgetGuide"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\yizhen\flutter_apps\FlutterWidgetGuide"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/annshsingh/Desktop/flutter/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.13"
-export "FLUTTER_BUILD_NUMBER=14"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.14"
+export "FLUTTER_BUILD_NUMBER=16"
